0eeb82be3e8be4fb86a9ed185a91740e00801b77
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-11 12:56:57 +0300

    Rendered dog images on the page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+
+    //function to fetch dog
+    function displayDogs(){
+
+        const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+        fetch(imgUrl)
+        .then(res => res.json())
+        .then(data =>renderImages(data.message))
+
+    }
+
+    function renderImages(images){
+        
+        const renderDiv= document.getElementById("dog-image-container");
+        const imagesContainer = document.getElementById("dog-images");
+
+        images.forEach(imageUrl =>{
+            
+        const listDogs = document.createElement("li");
+        listDogs.innerHTML=`<img src=${imageUrl}>`;
+        imagesContainer.appendChild(listDogs);
+    })
+
+
+       
+        renderDiv.appendChild(imagesContainer);
+    }
+
+    displayDogs();
+});
