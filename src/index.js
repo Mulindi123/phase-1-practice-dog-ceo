12b042182d81fa83827e3280a6485f4fd6f395b2
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-11 14:26:08 +0300

    Rendered dog breeds on the DOM

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
         fetch(imgUrl)
         .then(res => res.json())
         .then(data =>renderImages(data.message))
+        .catch(error=> console.log(error))
 
     }
 
@@ -17,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const imagesContainer = document.getElementById("dog-images");
 
         images.forEach(imageUrl =>{
-            
+
         const listDogs = document.createElement("li");
         listDogs.innerHTML=`<img src=${imageUrl}>`;
         imagesContainer.appendChild(listDogs);
@@ -29,4 +30,35 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     displayDogs();
+
+    // function to fetch dog breeds
+function dogBreeds (){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(res =>res.json())
+    .then(data => renderBreeds(data.message))
+    .catch(error=>console.log(error))
+
+}
+
+// render breeds on the web page
+function renderBreeds(breeds){
+    const breedUl= document.getElementById("dog-breeds");
+    breedDiv.appendChild(breedUl)
+   for(const breed in breeds){
+   
+    const breedLi= document.createElement("li")
+      breedLi.textContent = breed;
+
+        breedUl.appendChild(breedLi);
+    }
+    
+}
+const breedDiv = document.createElement("div");
+    document.body.append(breedDiv);
+    
+
+
+    dogBreeds()
 });
+
